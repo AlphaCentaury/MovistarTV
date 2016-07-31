cf99bf4b140afafb1fb2b5f562c127a63accc082
AlphaCentaury <Developer@DellXPS8900>
2016-07-31 04:14:47 +0200

    Removed DVB prefix

--- 1.5 'Kruger 60'/ChannelList/Properties/InvariantTexts.Designer.cs
+++ 1.5 'Kruger 60'/ChannelList/Properties/InvariantTexts.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Project.DvbIpTv.ChannelList.Properties {
+namespace Project.IpTv.ChannelList.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Project.DvbIpTv.ChannelList.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project.DvbIpTv.ChannelList.Properties.InvariantTexts", typeof(InvariantTexts).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project.IpTv.ChannelList.Properties.InvariantTexts", typeof(InvariantTexts).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
