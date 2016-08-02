6ffe09fad01a311e2184fe1a5a22d61ba454f732
AlphaCentaury <Developer@DellXPS8900>
2016-08-03 00:19:34 +0200

    New internal tool: ChannelLogos

---
+++ 1.5 'Kruger 60'/InternalTools/ChannelLogos/WebClientEx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Project.IpTv.Internal.Tools.ChannelLogos
+{
+    public class WebClientEx : WebClient
+    {
+        public WebClientEx(CookieContainer container)
+        {
+            this.container = container;
+        }
+
+        public CookieContainer CookieContainer
+        {
+            get { return container; }
+            set { container = value; }
+        }
+
+        private CookieContainer container;
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest r = base.GetWebRequest(address);
+            var request = r as HttpWebRequest;
+            if (request != null)
+            {
+                request.CookieContainer = container;
+            }
+            return r;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request, IAsyncResult result)
+        {
+            WebResponse response = base.GetWebResponse(request, result);
+            ReadCookies(response);
+            return response;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            try
+            {
+                WebResponse response = base.GetWebResponse(request);
+                ReadCookies(response);
+                return response;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void ReadCookies(WebResponse r)
+        {
+            var response = r as HttpWebResponse;
+            if (response != null)
+            {
+                CookieCollection cookies = response.Cookies;
+                container.Add(cookies);
+            }
+        }
+    }
+}
