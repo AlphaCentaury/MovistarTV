fb3d91433fbb7d4e21faad817b0d319204237737
AlphaCentaury <AlphaCentaury@users.noreply.github.com>
2016-08-28 00:40:39 +0200

    Added Services.EPG.Serialization
    Code obtained from old, unreferenced project Services.EPG

---
+++ 1.5 'Kruger 60'/Services.EPG.Serialization/TvAnytime/Common.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (C) 2015-2016, Codeplex/GitHub user AlphaCentaury
+// All rights reserved, except those granted by the governing license of this software. See 'license.txt' file in the project root for complete license information.
+
+namespace IpTviewr.Services.EPG.Serialization.TvAnytime
+{
+    internal class Common
+    {
+        public const string DefaultXmlNamespace = "urn:tva:metadata:2007";
+        public const string Mpeg7XmlNamespace = "urn:tva:mpeg7:2005";
+        public const string TvaExtendedNamespace = "urn:tva:metadata:extended:2007";
+    } // Common
+} // namespace
---
+++ 1.5 'Kruger 60'/Services.EPG.Serialization/TvAnytime/ExtendedPurchaseItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace IpTviewr.Services.EPG.Serialization.TvAnytime
+{
+    [XmlType(TypeName= "ExtendedPurchaseItemType", Namespace = Common.TvaExtendedNamespace)]
+    public class ExtendedPurchaseItem: TvaMain
+    {
+        [XmlElement("ProgramDescription", Namespace = Common.DefaultXmlNamespace)]
+        public TvaProgramDescription ProgramDescription
+        {
+            get;
+            set;
+        } // ProgramDescription
+    } // ExtendedPurchaseItem
+} // namespace
