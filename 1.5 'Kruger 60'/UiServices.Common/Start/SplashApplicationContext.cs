acdc635ad07064ffd20c116eac8ec5c749dfae7f
AlphaCentaury <AlphaCentaury@users.noreply.github.com>
2016-09-07 00:07:31 +0200

    SplashApplicationContext
    Added exception handling when creating/displaying the main form.

--- 1.5 'Kruger 60'/UiServices.Common/Start/SplashApplicationContext.cs
+++ 1.5 'Kruger 60'/UiServices.Common/Start/SplashApplicationContext.cs
@@ -295,15 +295,42 @@ namespace IpTviewr.UiServices.Common.Start
             worker.Dispose();
             worker = null;
 
-            if ((!isOk) || (e.Cancelled) || (e.Error != null) || ((mainForm = GetMainForm()) == null))
+            var close = (!isOk) || (e.Cancelled) || (e.Error != null);
+            if (!close)
+            {
+                close = true;
+                try
+                {
+                    mainForm = GetMainForm();
+                    close = (mainForm == null);
+                }
+                catch (Exception ex)
+                {
+                    DisplayException(Properties.Splash.ExceptionGetMainForm, false, true, ex);
+                } // try-catch
+            } // if
+
+            if (!close)
+            {
+                close = true;
+                try
+                {
+                    StartMainForm(mainForm);
+                    close = false;
+                }
+                catch (Exception ex)
+                {
+                    DisplayException(Properties.Splash.ExceptionStartMainForm, false, true, ex);
+                    close = true;
+                } // try-catch
+            } // if
+
+            if (close)
             {
                 splashScreen.Close();
                 ExitThread();
-
                 return;
             } // if
-
-            StartMainForm(mainForm);
         } // Worker_RunWorkerCompleted
 
         #endregion
