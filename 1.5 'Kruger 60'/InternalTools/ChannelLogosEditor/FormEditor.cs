5ff57fe788fd0fbfaf698e4f23a546eaa5d523e0
AlphaCentaury <sarechaga@hotmail.com>
2019-06-30 17:36:01 +0200

    Internal tools: added ChanelLogosEditor (stub)

---
+++ 1.5 'Kruger 60'/InternalTools/ChannelLogosEditor/FormEditor.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (C) 2014-2019, GitHub/Codeplex user AlphaCentaury
+// 
+// All rights reserved, except those granted by the governing license of this software.
+// See 'license.txt' file in the project root for complete license information.
+// 
+// http://www.alphacentaury.org/movistartv https://github.com/AlphaCentaury
+
+using System.Windows.Forms;
+
+namespace IpTviewr.Internal.Tools.ChannelLogosEditor
+{
+    public partial class FormEditor : Form
+    {
+        public FormEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 1.5 'Kruger 60'/InternalTools/ChannelLogosEditor/Program.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (C) 2014-2019, GitHub/Codeplex user AlphaCentaury
+// 
+// All rights reserved, except those granted by the governing license of this software.
+// See 'license.txt' file in the project root for complete license information.
+// 
+// http://www.alphacentaury.org/movistartv https://github.com/AlphaCentaury
+
+using System;
+using System.Windows.Forms;
+
+namespace IpTviewr.Internal.Tools.ChannelLogosEditor
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormEditor());
+        } // Main
+    } // class Program
+} // namespace
