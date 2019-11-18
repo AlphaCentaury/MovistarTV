64c904a4ecae42a07ea5aa3e138559beca4f1aa7
AlphaCentaury <sarechaga@hotmail.com>
2019-11-19 00:13:52 +0100

    movistar+ IpTVProvider

---
+++ 1.5 'Kruger 60'/Assemblies/IpTv.MovistarPlus/Serialization/MovistarPlusConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IpTviewr.IpTvServices.MovistarPlus.Serialization
+{
+    [Serializable]
+    public class MovistarPlusConfig
+    {
+        public Opch Opch { get; set; }
+        public IDictionary<string, string> Environment;
+        public IDictionary<string, IDictionary<string, string>> Variables;
+    } // class MovistarPlusConfig
+} // namespace
---
+++ 1.5 'Kruger 60'/Assemblies/IpTv.MovistarPlus/Serialization/Opch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace IpTviewr.IpTvServices.MovistarPlus.Serialization
+{
+    public class Opch
+    {
+        public string MulticastAddress
+        {
+            get;
+            set;
+        } // MulticastAddress
+
+        public int MulticastPort
+        {
+            get;
+            set;
+        } // MulticastPort
+
+        public IDictionary<string, string> Default
+        {
+            get;
+            set;
+        } // DefaultValues
+    } // Opch
+} // namespace
