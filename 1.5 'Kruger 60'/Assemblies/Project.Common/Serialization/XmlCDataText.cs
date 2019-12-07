c2f497143929247b63084d0186bacd6d3fa6462a
AlphaCentaury <sarechaga@hotmail.com>
2019-12-07 01:06:06 +0100

    XmlCDataText: implemented IEquatable & IComparable & static operators

--- 1.5 'Kruger 60'/Assemblies/Project.Common/Serialization/XmlCDataText.cs
+++ 1.5 'Kruger 60'/Assemblies/Project.Common/Serialization/XmlCDataText.cs
@@ -6,6 +6,7 @@
 // http://www.alphacentaury.org/movistartv https://github.com/AlphaCentaury
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 using System.Xml.Schema;
@@ -14,7 +15,7 @@ using System.Xml.Serialization;
 namespace IpTviewr.Common.Serialization
 {
     [Serializable]
-    public class XmlCDataText : IXmlSerializable
+    public class XmlCDataText : IXmlSerializable, IEquatable<XmlCDataText>, IEquatable<string>, IComparable<XmlCDataText>, IComparable<string>, IComparable
     {
         public XmlCDataText()
         {
@@ -56,5 +57,119 @@ namespace IpTviewr.Common.Serialization
 
         public static implicit operator XmlCDataText(string text) => new XmlCDataText(text);
         public static implicit operator string(XmlCDataText xmlCData) => xmlCData.Text;
+
+        #region Equality members
+
+        public bool Equals(XmlCDataText other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Text, other.Text, StringComparison.CurrentCulture);
+        } // Equals
+
+        public bool Equals(string other)
+        {
+            if (other is null) return false;
+            return string.Equals(Text, other, StringComparison.CurrentCulture);
+        } // Equals
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || (obj is XmlCDataText other && Equals(other)) || (obj is string text && Equals(text));
+        } // Equals
+
+        public override int GetHashCode()
+        {
+            return (Text != null ? StringComparer.CurrentCulture.GetHashCode(Text) : 0);
+        } // GetHashCode
+
+        public static bool operator ==(XmlCDataText left, XmlCDataText right)
+        {
+            if (ReferenceEquals(left, right)) return true;
+            if (left is null) return false;
+            return left.Equals(right);
+        } // operator ==
+
+        public static bool operator !=(XmlCDataText left, XmlCDataText right)
+        {
+            return left != right;
+        } // operator !=
+
+        public static bool operator ==(XmlCDataText left, string right)
+        {
+            return !(left is null) && left.Equals(right);
+        } // operator ==
+
+        public static bool operator !=(XmlCDataText left, string right)
+        {
+            return left != right;
+        } // operator !=
+
+        public static bool operator ==(string left, XmlCDataText right)
+        {
+            return !(right is null) && right.Equals(left);
+        } // operator ==
+
+        public static bool operator !=(string left, XmlCDataText right)
+        {
+            return left != right;
+        } // operator !=
+
+        #endregion
+
+        #region Relational members: XmlCDataText
+
+        public int CompareTo(XmlCDataText other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (other is null) return 1;
+            return string.Compare(Text, other.Text, StringComparison.CurrentCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is null) return 1;
+            if (ReferenceEquals(this, obj)) return 0;
+            switch (obj)
+            {
+                case XmlCDataText other:
+                    return CompareTo(other);
+                case string text:
+                    return CompareTo(text);
+                default:
+                    throw new ArgumentException($"CompareTo: Object must be of type {nameof(XmlCDataText)} -or- {nameof(String)}");
+            } // switch
+        } // CompareTo
+
+        public static bool operator <(XmlCDataText left, XmlCDataText right)
+        {
+            return Comparer<XmlCDataText>.Default.Compare(left, right) < 0;
+        } // operator <
+
+        public static bool operator >(XmlCDataText left, XmlCDataText right)
+        {
+            return Comparer<XmlCDataText>.Default.Compare(left, right) > 0;
+        } // operator >
+
+        public static bool operator <=(XmlCDataText left, XmlCDataText right)
+        {
+            return Comparer<XmlCDataText>.Default.Compare(left, right) <= 0;
+        } // operator <=
+
+        public static bool operator >=(XmlCDataText left, XmlCDataText right)
+        {
+            return Comparer<XmlCDataText>.Default.Compare(left, right) >= 0;
+        } // operator >=
+
+        #endregion
+
+        #region Relational members: string
+
+        public int CompareTo(string other)
+        {
+            return (other is null) ? 1 : string.Compare(Text, other, StringComparison.CurrentCulture);
+        } // CompareTo
+
+        #endregion
     } // class XmlCDataText
 } // namespace
